35c811676f69b1fd40ffd8d1355626572f50bf9d
Dineshkumarsrini <107687653+Dineshkumarsrini@users.noreply.github.com>
2023-02-03 13:02:27 +0530

    add library changes
    add library changes

--- app/src/main/java/com/example/sdkapplication/MainActivity.java
+++ app/src/main/java/com/example/sdkapplication/MainActivity.java
@@ -3,17 +3,12 @@ package com.example.sdkapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.tv_center);
-        textView.setText("Testing Sample SDK");
-        Log.d("<<<","Testing(Triggering) Automatic Build");
     }
 }
